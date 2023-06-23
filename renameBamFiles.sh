b878b8a7ca92b1a1178ad7d0906160db8c14aaf7
Rachel Schwartz <51383714+rachadele@users.noreply.github.com>
2023-06-23 09:20:30 -0700

    Update and rename rename bam files to renameBamFiles.sh

--- renameBamFiles.sh
+++ renameBamFiles.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 for i in $(find SRR*/SRR*/* -type d); do 
 cd $i; f=$(echo $i | sed -E s,SRR.{9},, | sed s,/,_,); ls | while read line; do mv $line $(echo $line | sed "s,bamtofastq,$f,"); done; cd ../../../; done
